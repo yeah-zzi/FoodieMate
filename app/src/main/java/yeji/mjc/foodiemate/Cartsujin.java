4812233e0a413caa771b035162bd424b8d46a014
82109 <kimgeulim>
2023-04-22 02:02:35 +0900

    장바구니 추가 및 상단바 고정

--- app/src/main/java/yeji/mjc/foodiemate/Cartsujin.java
+++ app/src/main/java/yeji/mjc/foodiemate/Cartsujin.java
@@ -3,17 +3,46 @@ package yeji.mjc.foodiemate;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import yeji.mjc.foodiemate.cart.FoodAdapter;
+import yeji.mjc.foodiemate.cart.FoodItem;
+import yeji.mjc.foodiemate.comunity.TipAdapter;
+import yeji.mjc.foodiemate.comunity.TipItem;
+
 public class Cartsujin extends Fragment {
+
+    public RecyclerView recyclerView;
+    public RecyclerView.Adapter adapter_cart;
+    public ArrayList<FoodItem> foodItems = new ArrayList<FoodItem>();
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cartsujin, container, false);
+        View view = inflater.inflate(R.layout.fragment_cartsujin, container, false);
+        recyclerView = view.findViewById(R.id.foodListRV);
+        recyclerView.setHasFixedSize(true);
+        return view;
+    }
+
+    public void onStart(){
+        super.onStart();
+
+        for(int i=0;i<9;i++){
+            foodItems.add(new FoodItem("오징어", "3개",R.drawable.bread,true));
+        }
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new FoodAdapter(foodItems));
     }
 }
