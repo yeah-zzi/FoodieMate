6a2e2014aec9db8036d51a3a8fb66a6c28760ab9
82109 <kimgeulim>
2023-04-21 23:35:29 +0900

    커뮤니티 메뉴 애니메이션 가동하게 자바 코드 추가

--- app/src/main/java/yeji/mjc/foodiemate/Comsujin.java
+++ app/src/main/java/yeji/mjc/foodiemate/Comsujin.java
@@ -1,5 +1,6 @@
 package yeji.mjc.foodiemate;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class Comsujin extends Fragment {
+import yeji.mjc.foodiemate.frige.Frigesujin;
+
+public class Comsujin extends Fragment implements View.OnClickListener{
+
+    ColorStateList def;
+    TextView life_info,tip,food_battle,select;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        //커뮤티니 메뉴바 구현
         View view = inflater.inflate(R.layout.tip_main, container, false);
+        life_info = view.findViewById(R.id.life_info);
+        tip = view.findViewById(R.id.tip);
+        food_battle = view.findViewById(R.id.food_battle);
+
+        life_info.setOnClickListener(this);
+        tip.setOnClickListener(this);
+        food_battle.setOnClickListener(this);
+
+        select = view.findViewById(R.id.select);
+        def = tip.getTextColors();
+
+
         return view;
+    }
+    //커뮤니티 메뉴바 선택 배경 움직이는 애니메이션
+    public void onClick(View view){
+        if(view.getId() == R.id.life_info){
+            select.animate().x(70).setDuration(100);
+        }else if(view.getId()==R.id.tip){
+            int size = tip.getWidth() + 75;
+            select.animate().x(size).setDuration(100);
+        }else if(view.getId()==R.id.food_battle){
+            int size = tip.getWidth() * 2 + 85;
+            select.animate().x(size).setDuration(100);
+        }
     }
 }
