46d393226ec6ba5f52936e0f6cc4b11586500fc4
hyerim23 <119495992+hyerim23@users.noreply.github.com>
2023-04-23 22:04:57 +0900

    집밥대결 fragment 추가, cart drawable 수정

--- app/src/main/java/yeji/mjc/foodiemate/comunity/fight_fragment.java
+++ app/src/main/java/yeji/mjc/foodiemate/comunity/fight_fragment.java
@@ -18,6 +18,7 @@ public class fight_fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fight_main, container, false);
+
         return view;
     }
 
