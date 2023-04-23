046838be3bcb9d1d9220703259725ae1dff7bf73
82109 <kimgeulim>
2023-04-23 20:11:59 +0900

    커뮤니티 리사이클러뷰 사이즈 조정 및 초기화면 설정

--- app/src/main/java/yeji/mjc/foodiemate/comunity/Life_Fragment.java
+++ app/src/main/java/yeji/mjc/foodiemate/comunity/Life_Fragment.java
@@ -30,6 +30,14 @@ public class Life_Fragment extends Fragment {
                 R.drawable.post1));
         list.add(new LifePostItem("좁아도 OK! 똑똑하게 싱크대 쓰는 법", "걸어서 사용할 수 있는 것은 되도록 걸어서 사용하는 것이 주방을 넓게 사용하는 방법 중 하나인데요. 조리도구나 키친타올 등 걸 수 있는 것들은 모두 걸 수 있도록 아이디어를 내면 좋아요.",
                 R.drawable.post1));
+        list.add(new LifePostItem("좁아도 OK! 똑똑하게 싱크대 쓰는 법", "걸어서 사용할 수 있는 것은 되도록 걸어서 사용하는 것이 주방을 넓게 사용하는 방법 중 하나인데요. 조리도구나 키친타올 등 걸 수 있는 것들은 모두 걸 수 있도록 아이디어를 내면 좋아요.",
+                R.drawable.post1));
+        list.add(new LifePostItem("좁아도 OK! 똑똑하게 싱크대 쓰는 법", "걸어서 사용할 수 있는 것은 되도록 걸어서 사용하는 것이 주방을 넓게 사용하는 방법 중 하나인데요. 조리도구나 키친타올 등 걸 수 있는 것들은 모두 걸 수 있도록 아이디어를 내면 좋아요.",
+                R.drawable.post1));
+        list.add(new LifePostItem("좁아도 OK! 똑똑하게 싱크대 쓰는 법", "걸어서 사용할 수 있는 것은 되도록 걸어서 사용하는 것이 주방을 넓게 사용하는 방법 중 하나인데요. 조리도구나 키친타올 등 걸 수 있는 것들은 모두 걸 수 있도록 아이디어를 내면 좋아요.",
+                R.drawable.post1));
+        list.add(new LifePostItem("좁아도 OK! 똑똑하게 싱크대 쓰는 법", "걸어서 사용할 수 있는 것은 되도록 걸어서 사용하는 것이 주방을 넓게 사용하는 방법 중 하나인데요. 조리도구나 키친타올 등 걸 수 있는 것들은 모두 걸 수 있도록 아이디어를 내면 좋아요.",
+                R.drawable.post1));
 
         recyclerView = (RecyclerView) view.findViewById(R.id.life_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
